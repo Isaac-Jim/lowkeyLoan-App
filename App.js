21c98a0a52818b3e0abfce8da617a07b7076672b
moimeme28 <oseiemmaadobea@gmail.com>
2024-08-16 16:11:58 +0000

    everything is well with the three pages

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import SplashScreen from "./screens/SplashScreen";
 import SignInScreen from "./screens/SignInScreen";
 import SignUpScreen from "./screens/SignUpScreen";
+import SplashScreen from "./screens/SplashScreen"
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => (
   <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash">
-        <Stack.Screen name="Splash" componenent={SplashScreen} />
+        <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="SignIn" component={SignInScreen} />
       </Stack.Navigator>
